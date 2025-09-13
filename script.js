a12dc0529642d82c216c958962f14599ad861185
Richard Liu <richardadesigner@gmail.com>
2025-09-13 02:08:31 -0700

    add search function

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ document.getElementById("deletebutton").addEventListener("click", function () {
         }
     }
 })
+
+document.getElementById("searchButton").addEventListener("click", function () {
+
+    document.getElementById("display").innerHTML = ""
+    var resultContainer = document.createElement("ul")
+    var targetItem = document.getElementById("input1").value;
+    for (var i = 0; i < list.length; i = i + 1) {
+        if (list[i].includes(targetItem)) {
+            var result = document.createElement("li")
+            result.innerHTML = filterHTML(list[i]);
+            resultContainer.appendChild(result);
+        }
+    }
+    document.getElementById("display").appendChild(resultContainer);
+})
