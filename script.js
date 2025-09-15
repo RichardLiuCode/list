a4967750dd2a51557fcd261655045fb74b30e69c
Richard Liu <richardadesigner@gmail.com>
2025-09-14 17:42:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.getElementById("addItem").addEventListener("click", function () {
     var inputItem = filterHTML(document.getElementById("input1").value);
     if (inputItem != "") {
         list.push(inputItem);
-        document.getElementById("display").innerHTML = getHTMlList(list)
+        document.getElementById("display").innerHTML = getHTMLList(list)
     }
 })
 document.getElementById("deletebutton").addEventListener("click", function () {
@@ -13,7 +13,7 @@ document.getElementById("deletebutton").addEventListener("click", function () {
     for (var i = 0; i < list.length; i = i + 1) {
         if (list[i] == deleteItem) {
             list.splice(i, 1)
-            document.getElementById("display").innerHTML = getHTMlList(list)
+            document.getElementById("display").innerHTML = getHTMLList(list)
         }
     }
 })
